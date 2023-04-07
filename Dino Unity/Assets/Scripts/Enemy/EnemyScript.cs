5b8bc901f5fd7311c44339a65586638230ca98a4
Ryan <36418964+bruhryan10@users.noreply.github.com>
2023-04-07 16:50:36 -0700

    Worked on powerups

--- Dino Unity/Assets/Scripts/Enemy/EnemyScript.cs
+++ Dino Unity/Assets/Scripts/Enemy/EnemyScript.cs
@@ -7,13 +7,20 @@ public class EnemyScript : MonoBehaviour
     public float startTimer;
     public bool startChase;
     public GameObject Enemy;
+    PowerUp powerUp;
     void Start()
     {
+        //powerUp = GameObject.FindGameObjectWithTag("Meat").GetComponent<PowerUp>();
         startTimer = 5f;
     }
 
     void Update()
     {
+        //powerUp = GameObject.FindGameObjectWithTag("Meat").GetComponent<PowerUp>();
+        if (powerUp.startDelay)
+        {
+            return;
+        }
         startTimer -= Time.deltaTime;
         if (startTimer <= 0)
         {
--- Dino Unity/Assets/Scripts/Power Up/PowerUp.cs
+++ Dino Unity/Assets/Scripts/Power Up/PowerUp.cs
@@ -7,34 +7,99 @@ public class PowerUp : MonoBehaviour
     public float speedBoost = 2f;
     public float jumpBoost = 2f;
     public float duration = 5f;
+    public bool startJump;
+    public bool startSpeed;
+    public bool startDelay;
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            PlayerMovement player = other.GetComponent<PlayerMovement>();
-            player.moveSpeed += speedBoost;
-            player.jumpSpeed+= jumpBoost;
-            StartCoroutine(PowerUpTimer(player));
-            Destroy(gameObject);
-        }
-       
-    }
-    IEnumerator PowerUpTimer(PlayerMovement player)
-    {
-        yield return new WaitForSeconds(duration);
-        player.moveSpeed -= speedBoost;
-        player.jumpSpeed -= jumpBoost;
-    }
-    // Start is called before the first frame update
+    public GameObject powerUp;
+    PlayerMovement playerMovement;
     void Start()
     {
-        
+        playerMovement = GameObject.Find("Player").GetComponent<PlayerMovement>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (startJump)
+        {
+            duration -= Time.deltaTime;
+            if (duration < 0)
+            {
+                endJump();
+            }
+        }
+        if (startSpeed)
+        {
+            duration -= Time.deltaTime;
+            if (duration < 0)
+            {
+                endSpeed();
+            }
+        }
+        if (startDelay)
+        {
+            duration -= Time.deltaTime;
+            if (duration < 0)
+            {
+                endDelay();
+            }
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && collision.otherCollider.tag == "Shoes")
+        {
+            JumpUp();
+            Debug.Log("Shoes");
+
+        }
+        if (collision.gameObject.tag == "Player" && collision.otherCollider.tag == "Riding")
+        {
+            Debug.Log("Riding");
+            SpeedUp();
+        }
+        if (collision.gameObject.tag == "Player" && collision.otherCollider.tag == "Meat")
+        {
+            Debug.Log("Meat");
+            DelayEnemy();
+        }
+    }
+    private void SpeedUp()
+    {
+        powerUp.transform.position = new Vector3(-66, 13);
+        startSpeed = true;
+        playerMovement.moveSpeed += speedBoost;
+    }
+    private void endSpeed()
+    {
+        startSpeed = false;
+        duration = 5f;
+        playerMovement.moveSpeed -= speedBoost;
+        Destroy(gameObject);
+    }
+    private void JumpUp()
+    {
+        powerUp.transform.position = new Vector3(-66, 13);
+        startJump = true;
+        playerMovement.jumpSpeed += jumpBoost;
+    }
+    private void endJump()
+    {
+        startJump = false;
+        duration = 5f;
+        playerMovement.jumpSpeed -= jumpBoost;
+        Destroy(gameObject);
+    }
+    private void DelayEnemy()
+    {
+        powerUp.transform.position = new Vector3(-66, 13);
+        startDelay = true;
+    }
+    private void endDelay()
+    {
+        startDelay = false;
+        duration = 5f;
+        Destroy(gameObject);
     }
 }
