dccf987de2b21d9ceb9f9c31dd83b7ee3c82138e
bruhryan10 <ryanthebot10@gmail.com>
2024-06-19 16:44:28 -0700

    Added project

---
+++ Dino Unity/Assets/Scripts/UI/PlainsUI.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class PlainsUI : MonoBehaviour
+{
+    PlayerMovement playerMovement;
+    [SerializeField] Canvas MainUI;
+    [SerializeField] GameObject start;
+    [SerializeField] GameObject continueButton;
+    [SerializeField] float runTimer;
+    void Start()
+    {
+        playerMovement = GameObject.Find("Player").GetComponent<PlayerMovement>();
+        runTimer = 4f;
+        Time.timeScale = 1f;
+        MainUI.enabled = true;
+        continueButton.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (start.activeInHierarchy)
+            runTimer -= Time.deltaTime;
+        if (runTimer <= 0)
+            start.SetActive(false);
+        if (playerMovement.endLevel == true)
+        {
+            Time.timeScale = 0f;
+            continueButton.SetActive(true);
+        }
+    }
+    public void LoadTundraEasy()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("TundraEasy");
+    }
+}
